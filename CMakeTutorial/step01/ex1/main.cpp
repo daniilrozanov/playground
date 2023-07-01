254168dc807200dcd2e89e7f89b4cae7ead72890
Даниил Розанов <chell@MacBook-Pro-Daniil.local>
2023-07-02 01:57:58 +0300

    Step 1 done

---
+++ CMakeTutorial/step01/ex1/main.cpp
@@ -0,0 +1,28 @@
+#include "config.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string_view>
+
+#define STRING(s) #s
+
+void version() {
+#ifdef ex1_VERSION_MAJOR
+#ifdef ex1_VERSION_MINOR
+  std::cout << ex1_VERSION_MAJOR << "." << ex1_VERSION_MINOR << std::endl;
+#endif
+#else
+  std::cout << "Version not described" << std::endl;
+#endif
+}
+
+int main(int argc, char **argv) {
+  version();
+  if (argc < 2) {
+    std::cout << "enter number as argument: " << argv[0] << " <number>"
+              << std::endl;
+    return 1;
+  }
+  int n = std::atoi(argv[1]);
+  std::cout << n * n << std::endl;
+  return 0;
+}
