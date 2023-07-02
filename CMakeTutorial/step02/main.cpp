8820d3eba9de8096ce4886d7d1513eefcd2598a6
Daniil Rozanov <daniilrozzanov@gmail.com>
2023-07-02 04:03:52 +0300

    Step 2. Added library's compile option

--- CMakeTutorial/step02/main.cpp
+++ CMakeTutorial/step02/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv) {
   int res, n1, n2;
   if (argc != 4) {
-    std::cout << "Usage: " << argv[0] << "<num1> <op> <num2>" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <num1> <op> <num2>" << std::endl;
     return 1;
   }
   n1 = std::atoi(argv[1]);
@@ -18,10 +18,10 @@ int main(int argc, char **argv) {
     res = math::sub(n1, n2);
     break;
   case '*':
-    res = math::add(n1, n2);
+    res = math::sub(n1, n2);
     break;
   case '/':
-    res = math::add(n1, n2);
+    res = math::div(n1, n2);
     break;
   default:
     std::cout << "\"" << argv[2] << "\" is not supported";
