f441750b563f7c1e26741055c3d96942e0aab22a
Daniil Rozanov <daniilrozzanov@gmail.com>
2023-07-02 03:07:20 +0300

    Step 2 done

---
+++ CMakeTutorial/step02/math/src/constant.cpp
@@ -0,0 +1,7 @@
+namespace math {
+int add(int l, int r) { return l + r; }
+int mul(int l, int r) { return l * r; }
+int div(int l, int r) { return l / r; }
+int sub(int l, int r) { return l - r; }
+
+} // namespace math
